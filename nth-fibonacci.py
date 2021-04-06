62526279dd39f227819c6e5386d7ada607adb5f9
= <=>
2021-04-05 21:11:49 -0700

    Update fibonacci to use dynamic programming

--- nth-fibonacci.py
+++ nth-fibonacci.py
@@ -1,3 +1,5 @@
+results = {}
+
 def getNthFib(n):
 	fibs = [0]
 	count = 1
@@ -7,7 +9,12 @@ def getNthFib(n):
 	return fibs[n-1]
 
 def fib(n):
-	if n >= 3:
-		return fib(n-1) + fib(n-2)
-	else:
-		return 1
+    if n >= 3:
+        if n in results:
+            return results[n]
+
+        this_result = fib(n-1) + fib(n-2)
+        results[n] = this_result
+        return this_result
+    else:
+        return 1
